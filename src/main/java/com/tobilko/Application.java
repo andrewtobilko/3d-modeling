41a7cdfb4d15b52e0594612d42262a07d8bff1e7
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-10-09 14:28:01 +0300

    init

---
+++ src/main/java/com/tobilko/Application.java
@@ -0,0 +1,26 @@
+package com.tobilko;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+/**
+ * Created by Andrew Tobilko on 10/9/17.
+ */
+public final class Application extends javafx.application.Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setScene(new Scene(new FXMLLoader(new File("src/main/resources/application.fxml").toURI().toURL()).load()));
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
