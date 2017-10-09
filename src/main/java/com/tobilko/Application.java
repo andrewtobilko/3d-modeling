b229e13dc309d324621c48a0ea1e254fc6aed1a1
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-10-09 19:50:51 +0300

    drawLine()

--- src/main/java/com/tobilko/Application.java
+++ src/main/java/com/tobilko/Application.java
@@ -1,10 +1,12 @@
 package com.tobilko;
 
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.File;
 
 /**
  * Created by Andrew Tobilko on 10/9/17.
@@ -14,11 +16,21 @@ public final class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Root root = new FXMLLoader(new File("src/main/resources/application.fxml").toURI().toURL()).load();
-
-        stage.setScene(new Scene(new FXMLLoader(new File("src/main/resources/application.fxml").toURI().toURL()).load()));
+        Group root = new Group();
+        Canvas canvas = new Canvas(1000, 1000);
+        GraphicsContext context = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+        //drawLine(context);
+        stage.setScene(new Scene(root));
         stage.show();
 
+    }
+
+    private void drawLine(Point a, Point b) {
+//        context.setFill(Color.BLACK);
+//        context.setStroke(Color.BLACK);
+//        context.setLineWidth(5);
+//        context.strokeLine(a.getX(), a.getY(), b.getX(), b.getY());
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tobilko/Point.java
@@ -0,0 +1,14 @@
+package com.tobilko;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by Andrew Tobilko on 10/9/17.
+ */
+@Getter
+@RequiredArgsConstructor(staticName = "of")
+public final class Point {
+    private final int x;
+    private final int y;
+}
